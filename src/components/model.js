ab883fd434e21446fc17c9e15c748e42427cb9dc
Kathy Kang <kck3due@virginia.edu>
2020-10-03 18:43:46 -0400

    image

--- src/components/model.js
+++ src/components/model.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Button from 'react-bootstrap/Button';
 
 export default class Model extends Component {
     constructor(props) {
@@ -6,10 +7,22 @@ export default class Model extends Component {
         this.state = {
         }
     }
+
+    test(e){
+        fetch('https://console.echoAR.xyz/query?key=calm-mud-3261')
+        .then(response => response.json())
+        .then(data => console.log(data));
+
+        https://console.echoAR.xyz/query?key=calm-mud-3261&file="d93d28cf-fe47-4954-a9f0-052537ed20d1.glb"
+
+
+    }
     render() {
         return (
+
+            
             <div className={"model"}>
-                <h3>Choose something to send to your friend!</h3>
+                <Button variant="dark" onClick={()=>this.test()}>Choose something to send to your friend!</Button>{' '}
             </div>
         )
     }
