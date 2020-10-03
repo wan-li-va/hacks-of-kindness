e968fb791bafc20483cffa7bbedc84af6048fd7a
Andrew Wang <andrewxiaoyu0@gmail.com>
2020-10-03 18:27:38 -0400

    Upload audio

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,18 @@ import styles from '../styles/Home.module.css';
 import Header from './header.js';
 import Model from './model.js';
 import Message from './message.js';
+import * as firebase from 'firebase';
 
+var app = firebase.initializeApp({
+  apiKey: 'AIzaSyCblFEhn6FKGMYSaBeKLWLiojJaqxRABpI',
+  authDomain: 'wics-app-8c601.firebaseapp.com',
+  databaseURL: 'https://wics-app-8c601.firebaseio.com',
+  projectId: 'wics-app-8c601',
+  storageBucket: 'wics-app-8c601.appspot.com',
+  messagingSenderId: '1042898649667',
+  appId: '1:1042898649667:web:59908e029a4591ec7ccaf6',
+  measurementId: 'G-5F3SMMMTJ3',
+});
 
 
 
--- src/components/message.js
+++ src/components/message.js
@@ -3,18 +3,28 @@ import TextMessage from './TextMessage'
 import Recorder from './recorder'
 
 export default class Message extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        }
-    }
-    render() {
-        return (
-            <div className={"message"}>
-                <Recorder></Recorder>
-                <TextMessage></TextMessage>
-                <script src="https://code.responsivevoice.org/responsivevoice.js?key=B5AXjDdu"></script>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      audio: '',
+      entity: '',
+    };
+  }
+
+  updateState(key, value) {
+    let newState = {};
+    newState[key] = value;
+    this.setState(newState);
+    console.log('UPDATED STATE');
+  }
+  render() {
+    return (
+      <div className={'message'}>
+        <Recorder updateState={this.updateState.bind(this)}></Recorder>
+        <TextMessage></TextMessage>
+        <script src='https://code.responsivevoice.org/responsivevoice.js?key=B5AXjDdu'></script>
+      </div>
+    );
+  }
 }
