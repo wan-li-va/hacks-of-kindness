dfa70b9532241e54d7c12ebc8c2b5ece4cd5a741
Sion Kim <kim.gloria0417@gmail.com>
2020-10-03 18:43:40 -0400

    push

--- src/components/recorder.js
+++ src/components/recorder.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
-import styles from '../styles/Message.module.css'
+import '../styles/Recorder.module.css'
 import MicRecorder from 'mic-recorder-to-mp3'
-import 
 
 
 
@@ -56,12 +55,12 @@ export default class Recorder extends Component {
 
   render() {
     return (
-      <div>
-        <div class='btn-group' className={'playBar'}>
-        <button onClick={this.start} disabled={this.state.isRecording}>
+      <div className="recorder">
+        <div className="buttons">
+        <button id={'start'} onClick={this.start} disabled={this.state.isRecording}>
           Record
         </button>
-        <button onClick={this.stop} disabled={!this.state.isRecording}>
+        <button id={'stop'} onClick={this.stop} disabled={!this.state.isRecording}>
           Stop
         </button>
         </div>
